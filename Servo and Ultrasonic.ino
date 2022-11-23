c5b8e1e413696083f6652e2a66d9cbe875e87969
Keanu-Type <keanuberchestype0@gmail.com>
2022-11-23 19:05:10 +0800

    Direction fix

--- Servo and Ultrasonic.ino
+++ Servo and Ultrasonic.ino
@@ -1,7 +1,6 @@
 //------------- Servo -----------------
 #include <Servo.h>
 Servo Rotate;
-int degree;
 //--------------Ultrasonic-------------
 int Ear = 7; //Echo or Echo Pin
 int Shout = 8;   //Trig or Trigger pin
@@ -10,34 +9,27 @@ float length;
 float read = 0;
 float newread = 0;
 int direction;
-//-------------Motor------------------
 //motor one
-int enA = 11;
-int in1 = 10;
-int in2 = 9;
+int enA = 11; //int1 & int2
+int Go1 = 10; //int1
+int Back1 = 9;  //int2
 // motor two
-int enB = 3;
-int in3 = 6;
-int in4 = 5;
-bool signalON = true;
-bool signalOFF = false;
+int enB = 3;  //int3 & int4
+int Go2 = 5;  //int4
+int Back2 = 6;  //int3
 
 //===============================================================================================//
 void setup() {
-  // put your setup code here, to run once:
   pinMode(Shout, OUTPUT);
   pinMode(Ear, INPUT);
   Rotate.attach(2);
-  Serial.begin(9600);
-
   //Set all Motors 
   pinMode(enA, OUTPUT); //POWER
   pinMode(enB, OUTPUT); //POWER
-  pinMode(in1, OUTPUT); //DIRECTION A MOTOR A
-  pinMode(in2, OUTPUT); //DIRECTION B MOTOR A
-  pinMode(in3, OUTPUT); //DIRECTION C MOTOR B 
-  pinMode(in4, OUTPUT); //DIRECTION C MOTOR B
-  
+  pinMode(Go1, OUTPUT); //DIRECTION A MOTOR A
+  pinMode(Back1, OUTPUT); //DIRECTION B MOTOR A
+  pinMode(Back2, OUTPUT); //DIRECTION C MOTOR B 
+  pinMode(Go2, OUTPUT); //DIRECTION C MOTOR B
 }
 
 //------MOTOR DIRECTION CONTROLLER------//
@@ -48,21 +40,20 @@ int MotorHighLow(int ENABLE, int DISABLE, int ENABLE2, int DISABLE2){
   digitalWrite( DISABLE2, LOW);
 }
 //------MOTOR ENGINE ON AND OFF--------//
-bool Motor(bool signal){ //Engine ON
+bool Motor(bool signal, int speed){ //Engine ON
   if (signal==true){
-    analogWrite(enA, 70);
-    analogWrite(enB, 70);
-  }else if(!signal){ //Engine OFF
+    analogWrite(enA, speed);
+    analogWrite(enB, speed);
+  }else if(!signal){                //Engine OFF
     analogWrite(enA, 0);
     analogWrite(enB, 0);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
+    digitalWrite(Go1, LOW);
+    digitalWrite(Back1, LOW);
+    digitalWrite(Back2, LOW);
+    digitalWrite(Go2, LOW);
     delay(1000);
   };
 }
-
 //------ULTRASONIC READING---------
 int Ultrasonic(){
   digitalWrite(Shout, LOW);
@@ -86,9 +77,10 @@ int looping(){
       read = newread;
       direction = i;
     }
-  delay(10);
-  }   
-  delay(1000);
+  delay(5);
+  }
+  delay(500);
+  Rotate.write(90); //rotate servo back to 90 degree
   return direction;
 }
 
@@ -96,26 +88,31 @@ int looping(){
 void loop() {
   length = Ultrasonic();
   if (length < 15){
-    Motor(signalOFF); //STOP THE ENGINE
+    Motor(false, 0); //STOP THE ENGINE
+    //Go Back
+    MotorHighLow(Back2,Go2, Back1, Go1);
+    Motor(true, 40);
+    delay(1200);
+    Motor(false,0);
+    //GET BEST DIRECTION
     direction = looping(); //Rotate SERVO TO GET DATA IN SURROUNDING  
-    Rotate.write(90); //ROTATING SERVO BACK TO CENTER/90 degree
     //FIX MOBOT DIRECTION
     delay(1000);
     if (direction < 90){ //0-89 - RIGHT
-        MotorHighLow(in3, in4, in1, in2); //FIX MOBOT WHEEL FOR ROTATION
-        direction = 90 - direction;        
-    }else{               //90-180 - LEFT 
-        MotorHighLow(in2, in1, in4, in3 );  //FIX MOBOT WHEEL FOR ROTATION
-        direction = 180 - direction;
+        MotorHighLow(Back1, Go1, Go2, Back2 );  //FIX MOBOT WHEEL FOR ROTATION     
+    }else{               //90-180 - LEFT
+        MotorHighLow(Back2, Go2, Go1, Back1); //FIX MOBOT WHEEL FOR ROTATION       
     }
-    for (float i=0.00; i<(direction);i=i+0.1){
-      Motor(signalON); //ROTATE THE MOBOT
+    delay(500);
+    Motor(true, 60); //ROTATE THE MOBOT
+    for (int i=0; i<= 250;i=i+1){
       delay(2);
     }
-    Motor(signalOFF);
+    Motor(false, 0);
   }
+  
   //FORWARD
-  MotorHighLow(in1, in2, in4, in3); //MOTOR A FORWARD
-  Motor(signalON);        //Engine ON
-  delay(300);
+  MotorHighLow(Go1, Back1, Go2, Back2); //MOTOR A FORWARD
+  Motor(true, 55);        //Engine ON
+  delay(200);
 }
